a6987f3be0d5421576679c12ca0f311abf060ce3
Maximusk <mehdibeniche@gmail.com>
2015-12-04 06:28:51 +0100

    Correction des scripts

--- serv.sh
+++ serv.sh
@@ -2,24 +2,24 @@
 #This script is to use with "node" command#
 ###########################################
 
-# nodejs server.js 10000
-# nodejs server.js 10001
-# nodejs server.js 10002
-# nodejs server.js 10003
-# nodejs server.js 10004
-# nodejs server.js 10005
-# nodejs server.js 10006
-# nodejs server.js 10007
-# nodejs server.js 10008
+# nodejs server.js 10000&
+# nodejs server.js 10001&
+# nodejs server.js 10002&
+# nodejs server.js 10003&
+# nodejs server.js 10004&
+# nodejs server.js 10005&
+# nodejs server.js 10006&
+# nodejs server.js 10007&
+# nodejs server.js 10008&
 # nodejs server.js 10009
 
-node server.js 10000
-node server.js 10001
-node server.js 10002
-node server.js 10003
-node server.js 10004
-node server.js 10005
-node server.js 10006
-node server.js 10007
-node server.js 10008
+node server.js 10000&
+node server.js 10001&
+node server.js 10002&
+node server.js 10003&
+node server.js 10004&
+node server.js 10005&
+node server.js 10006&
+node server.js 10007&
+node server.js 10008&
 node server.js 10009
